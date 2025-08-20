22d4cca589e47713d7398593658cde464eb996d4
jlbuild <juliabuildbot@gmail.com>
2025-08-20 13:43:04 +0000

    SLEEF_jll build 3.9.0+0

--- src/wrappers/x86_64-linux-musl.jl
+++ src/wrappers/x86_64-linux-musl.jl
@@ -2,15 +2,13 @@
 export libsleef, libsleefdft, libsleefquad, libsleefscalar
 
 using CompilerSupportLibraries_jll
-using MPFR_jll
-using OpenSSL_jll
 JLLWrappers.@generate_wrapper_header("SLEEF")
 JLLWrappers.@declare_library_product(libsleef, "libsleef.so.3")
 JLLWrappers.@declare_library_product(libsleefdft, "libsleefdft.so.3")
 JLLWrappers.@declare_library_product(libsleefquad, "libsleefquad.so.3")
 JLLWrappers.@declare_library_product(libsleefscalar, "libsleefscalar.so.3")
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, MPFR_jll, OpenSSL_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
     JLLWrappers.@init_library_product(
         libsleef,
         "lib/libsleef.so",
