86a8488c811e2f94f9c936c12c0905eb4f2a3848
jlbuild <juliabuildbot@gmail.com>
2026-06-29 20:13:48 +0000

    SLEEF_jll build 3.9.0+1

--- src/SLEEF_jll.jl
+++ src/SLEEF_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SLEEF")
-JLLWrappers.@generate_main_file("SLEEF", UUID("63e82ce6-3d80-5af4-a84c-484b71ab98bb"))
+JLLWrappers.@generate_main_file("SLEEF", Base.UUID("63e82ce6-3d80-5af4-a84c-484b71ab98bb"))
 end  # module SLEEF_jll
